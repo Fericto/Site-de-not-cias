1add3f801c3d5abfc12125fff02e7d1af05eeed2
Fericto <150551962+Fericto@users.noreply.github.com>
2023-12-27 20:06:52 -0300

    Subir os arquivos do site de Notícias
    Adicionando um site para portfólio .

---
+++ script.js
@@ -0,0 +1,10 @@
+function semLogin () {
+    alert("Ainda não é possível de se realizar um login, mas fique a vontade para navegar pelo site.")
+}
+
+
+var chk = document.getElementById('chk')
+
+chk.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+})
