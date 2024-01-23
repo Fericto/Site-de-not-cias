a4abc470089ebf76afcbd35503311d41a5fb23ac
Felipe <lookingthegames@gmail.com>
2024-01-23 20:32:47 -0300

    branch melhorias: tentando criar metodo de refrash otimizado// erro com o botão

--- script.js
+++ script.js
@@ -4,7 +4,33 @@ function semLogin () {
 
 
 var chk = document.getElementById('chk')
+var tapreto = 1
 
 chk.addEventListener('change', () => {
     document.body.classList.toggle('dark');
+    tapreto *= -1
 })
+
+function abrirPag(a, coluna){
+    var localPag = document.querySelector("#container")
+    var pag = new XMLHttpRequest()
+    var elemento = document.querySelector("body")
+    elemento.className = a;
+    
+    if(coluna==1){
+        elemento.id = 'uma_coluna'
+    }else if (coluna==2){
+        elemento.id = 'duas_colunas'
+    }else if (coluna==3){
+        elemento.id = ''
+    }
+
+    pag.onreadystatechange = () =>{
+        if(pag.readyState == 4 && pag.status == 200){
+            localPag.innerHTML = pag.response
+        }
+    }
+
+    pag.open('GET', `../${a}.html`)
+    pag.send()
+}
